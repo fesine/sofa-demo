bc858984e6c1a8a950a1cab69d90f81416bc95dd
fesine <240944773@qq.com>
2020-01-10 10:51:24 +0800

    测试sofa boot 调用不同版本的dubbo服务。

---
+++ sofa-boot-demo/src/main/java/com/alibaba/dubbo/demo/dap/DapService.java
@@ -0,0 +1,14 @@
+package com.alibaba.dubbo.demo.dap;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/3/5
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/3/5
+ */
+public interface DapService {
+
+    void handle(String msg);
+}
---
+++ sofa-boot-demo/src/main/java/com/alibaba/dubbo/demo/test/IService.java
@@ -0,0 +1,14 @@
+package com.alibaba.dubbo.demo.test;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/3/4
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/3/4
+ */
+public interface IService {
+
+    void execute(String msg);
+}
